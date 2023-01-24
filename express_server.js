9a8bcd52e8e9079b032fc7cdda91fd2cf53cc7d8
jpared3s <jparedes.wall@gmail.com>
2023-01-24 15:25:32 -0500

    added update url

--- express_server.js
+++ express_server.js
@@ -68,6 +68,20 @@ app.post("/urls/:id/delete", (req, res) => {
   delete urlDatabase[id];
   res.redirect("/urls");
 });
+app.post("/urls/:id", (req, res) => {
+  const id = req.params.id;
+  res.redirect(`/urls/${id}`);
+});
+app.post("/urls/:id/edit", (req, res) => {
+  const id = req.params.id;
+  const newURL = req.body.NewLongURL;
+  for (let URL in urlDatabase) {
+    if (URL === id) {
+      urlDatabase[id] = newURL;
+      return res.redirect('/urls');
+    }
+  }
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
